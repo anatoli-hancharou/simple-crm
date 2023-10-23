81f42e173f8918e79aa32fcdbd0c56f290a48192
Anatoli Hancharou <anatoli.han@mail.ru>
2023-10-23 19:40:57 +0200

    Changed Description column type to Text

---
+++ simple-crm-server/db-migrator/migrations/20231023173342-ChangeDescriptionType.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.changeColumn(
+      'Customers',
+      'description',
+      {
+        type: Sequelize.TEXT,
+      }
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.changeColumn(
+      'Customers',
+      'description',
+      {
+        type: Sequelize.STRING,
+      }
+    );
+  }
+};
